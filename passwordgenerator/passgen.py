97333c647d9e4d9e2a75148664a5cee11f421a1c
playerdefault <swaraj.mohapatra@outlook.in>
2019-01-29 16:28:29 +0530

    Added virtualenv. Added basic complexity option

--- passwordgenerator/passgen.py
+++ passwordgenerator/passgen.py
@@ -1,20 +1,34 @@
 import json, random, click
 
-with open('words.json') as json_data:
-    words = json.load(json_data)
-    
-word_list = words["data"]
+@click.command()
+@click.option('--complex', default=0, help='Password complexity switch')
+def passgen(complex):
+    with open('words.json') as json_data:
+        words = json.load(json_data)
+        
+    word_list = words["data"]
 
-# create the password
+    pos1 = random.randrange(len(word_list))
+    string1 = word_list[pos1]
 
-pos1 = random.randrange(len(word_list))
-string1 = word_list[pos1]
+    pos2 = random.randrange(len(word_list))
+    string2 = word_list[pos2]
 
-pos2 = random.randrange(len(word_list))
-string2 = word_list[pos2]
+    pos3 = random.randrange(len(word_list))
+    string3 = word_list[pos3]
 
-pos3 = random.randrange(len(word_list))
-string3 = word_list[pos3]
+    if (complex == 0):
+        final = string1 + ' ' + string2 + ' ' + string3
+        print(final)
+    elif (complex == 1):
+        num1 = '_' + str(random.randrange(100)) + '_'
+        num2 = '_' + str(random.randrange(100)) + '_'
+        num3 = '_' +  str(random.randrange(100))
 
-final = string1 + ' ' + string2 + ' ' + string3
-print(final)
+        final = string1 + num1 + string2 + num2 + string3 + num3
+        print(final)
+    else:
+        print("Wrong parameter")
+
+if __name__ == '__main__':
+    passgen()
